8a1eab64725594cb95c77b1069804931cbc0a806
Vinicius Rezende <vinicius.rezende@rethink.dev>
2023-01-18 12:41:11 -0300

    Initial Commit

---
+++ WhatsApp Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsApp Clone
+//
+//  Created by Vinicius Rezende on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
